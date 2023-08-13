fee29b8951246930176e453976c1247397e7cd43
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-08-13 18:12:03 -0400

    get categories by id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -19,10 +19,26 @@ router.get('/categories', async (req, res) => {
 });
 
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/categories/:id', async (req, res) => {
+  // Find one category by its `id` value
+  try {
+    const singleCategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!singleCategoryData) {
+      res.status(404).json({ message: 'No category found' });
+      return;
+    }
+
+    console.log('Showing one category by ID.');
+    res.status(200).json(singleCategoryData);
+  } catch (err) {
+    console.error('Error fetching category by ID:', err);
+    res.status(500).json({ error: 'server error' });
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
