3cd0d45c643b31cffeaf988aa51b8862125b8150
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-08-13 18:18:12 -0400

    post categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -40,9 +40,28 @@ router.get('/categories/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/categories', async (req, res) => {
+  // Create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+
+    // If there are product IDs, create associations with ProductCategory
+    if (req.body.productIds && req.body.productIds.length > 0) {
+      const categoryIdArr = req.body.productIds.map((product_id) => ({
+        category_id: newCategory.id,
+        product_id,
+      }));
+      await ProductCategory.bulkCreate(categoryIdArr);
+    }
+
+    console.log('New category created:', newCategory.id);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    console.error('Error creating category:', err);
+    res.status(500).json({ error: 'server error' });
+  }
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
